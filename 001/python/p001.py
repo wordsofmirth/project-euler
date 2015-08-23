bd4de6b1fcfabd7ae603fed94739e951daa70df3
Kevin O'Riordan <kfor@compsoc.com>
2015-08-23 14:50:24 +0100

    Added code to use argv.
    For some reason I was using a hardcoded exponent or 130000 in p001.py.

--- 001/python/p001.py
+++ 001/python/p001.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python -O 
 
+import sys
+
 def gauss_sum (n):
-	return( (n * (n+1)) / 2)
+    return( (n * (n+1)) / 2)
 
 def gauss_sum_mod (n, m):
-	return (gauss_sum((n-1) / m) * m)
+    return (gauss_sum((n-1) / m) * m)
 
-n = pow(10, 130000)
-
+n = int(sys.argv[1])
 sum3 = gauss_sum_mod(n, 3)
 sum5 = gauss_sum_mod(n, 5)
 sum15 = gauss_sum_mod(n, 15)
